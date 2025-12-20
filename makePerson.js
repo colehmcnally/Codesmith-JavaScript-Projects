14b0f529e846b1d5f6d24e7f2279de9b30601851
colehmcnally <129011080+colehmcnally@users.noreply.github.com>
2025-12-20 17:03:02 -0500

    first OOP challenge

---
+++ makePerson.js
@@ -0,0 +1,14 @@
+function makePerson(name, age) {
+    const newUser = {}; // create a new user object
+    newUser.name = name; // add a name property to the object 
+    newUser.age = age; // add an age property to the object
+
+    return newUser; // return the new object
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
